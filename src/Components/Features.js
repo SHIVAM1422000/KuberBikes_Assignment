95cd258d574f79a07c54c49a2fef0e1cb66f8177
SHIVAM1422000 <shivamojha1422000@gmail.com>
2022-01-15 23:09:43 +0530

    Version 1.0

--- src/Components/Features.js
+++ src/Components/Features.js
@@ -42,9 +42,9 @@ function Features() {
 <div className="col-sm-6 col-lg-3  mt-4">
 
 <div className="card" style={{"width": "18rem"}}>
-<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRheDELlfXu9Ju-Qp-QtnSya3N2jiLuRwDqQHYRZsDDzNAAhgp8" className="card-img-top" alt="1" style={{"height":"50vh"}}/>
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_LHZpRl0VFhVVnD6wWCvtSrHq6_FfSrqsJ1QIEdWd4zDBmqE0" className="card-img-top" alt="1" style={{"height":"50vh"}}/>
 <div className="card-body">
-<h5 className="card-title">Low Maintainence</h5>
+<h5 className="card-title">No License</h5>
 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
 </div>
 </div>
@@ -54,9 +54,9 @@ function Features() {
 <div className="col-sm-6 col-lg-3  mt-4">
 
 <div className="card" style={{"width": "18rem"}}>
-<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRheDELlfXu9Ju-Qp-QtnSya3N2jiLuRwDqQHYRZsDDzNAAhgp8" className="card-img-top" alt="1" style={{"height":"50vh"}}/>
+<img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQVJI1u5XwnDshqbcVb3yh9EYeiVekjOlQ1JL_zZb-D6UrhIP6c" className="card-img-top" alt="1" style={{"height":"50vh"}}/>
 <div className="card-body">
-<h5 className="card-title">Low Maintainence</h5>
+<h5 className="card-title">No Registration</h5>
 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
 </div>
 </div>
